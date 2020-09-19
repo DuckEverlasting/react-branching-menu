48134d2b6417d46ab69b8c20bea63f4267773ae4
Matt Klein <mklein246@gmail.com>
2020-09-19 13:28:24 -0700

    removed babel. wasn't necessary.

--- webpack.config.js
+++ webpack.config.js
@@ -25,16 +25,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      },
-      {
           test: /\.tsx?$/,
           use: 'ts-loader',
           exclude: '/node_modules/'
